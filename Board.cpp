c8178eb62ec2bea2eb68fe9163aa2e6a8b8d5a15
VelinaSouroujon <velinasouroujon@gmail.com>
2025-05-06 02:03:09 +0300

    Add Board class - encapsulates 2D matrix that represents chess board

---
+++ Board.cpp
@@ -0,0 +1,34 @@
+#include <stdexcept>
+#include "Board.h"
+#include "CommonUtils.h"
+
+int Board::getBoardRowIdx(int row) const
+{
+	return getSize() - row;
+}
+
+int Board::getBoardColIdx(char col) const
+{
+	return col - Constants::MIN_COL_COORDINATE;
+}
+
+const Piece* Board::at(const ChessCoordinate& coordinate) const
+{
+	int rowIdx = getBoardRowIdx(coordinate.getRow());
+	int colIdx = getBoardColIdx(coordinate.getCol());
+
+	return board[rowIdx][colIdx];
+}
+
+Piece* Board::at(const ChessCoordinate& coordinate)
+{
+	int rowIdx = getBoardRowIdx(coordinate.getRow());
+	int colIdx = getBoardColIdx(coordinate.getCol());
+
+	return board[rowIdx][colIdx];
+}
+
+int Board::getSize() const
+{
+	return Constants::BOARD_SIZE;
+}
---
+++ Board.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Piece.h"
+#include "Constants.h"
+#include "ChessCoordinate.h"
+
+class Board
+{
+private:
+	Piece* board[Constants::BOARD_SIZE][Constants::BOARD_SIZE]{ nullptr };
+
+	int getBoardRowIdx(int row) const;
+	int getBoardColIdx(char col) const;
+
+public:
+	const Piece* at(const ChessCoordinate& coordinate) const;
+	Piece* at(const ChessCoordinate& coordinate);
+
+	int getSize() const;
+};
+
