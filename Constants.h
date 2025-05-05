bceaa5a2f1890fbad14f379bb430d2095b18c804
VelinaSouroujon <velinasouroujon@gmail.com>
2025-05-05 22:56:46 +0300

    Add chess related constants
    Create new header file and add the constants within a namespace

---
+++ Constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Constants
+{
+	const int BOARD_SIZE = 8;
+	const int MIN_ROW_COORDINATE = 1;
+	const char MIN_COL_COORDINATE = 'a';
+	const char MAX_COL_COORDINATE = MIN_COL_COORDINATE + BOARD_SIZE - 1;
+
+	const int MAX_NOTATION_LENGTH = 3;
+}
