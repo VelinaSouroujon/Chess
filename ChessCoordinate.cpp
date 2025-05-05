3b536e6d08dda67d41e232c030f30efedc4035cd
VelinaSouroujon <velinasouroujon@gmail.com>
2025-05-05 23:03:35 +0300

    Add ChessCoordinate class

---
+++ ChessCoordinate.cpp
@@ -0,0 +1,68 @@
+#include <stdexcept>
+#include "ChessCoordinate.h"
+#include "Constants.h"
+#include "CommonUtils.h"
+
+ChessCoordinate::ChessCoordinate(char col, int row)
+{
+	setRow(row);
+	setCol(col);
+}
+
+char ChessCoordinate::getCol() const
+{
+    return col;
+}
+
+int ChessCoordinate::getRow() const
+{
+    return row;
+}
+
+void ChessCoordinate::setCol(char value)
+{
+	if (!CommonUtils::isInRange(value, Constants::MIN_COL_COORDINATE, Constants::MAX_COL_COORDINATE))
+	{
+		throw std::out_of_range("Column is out of range");
+	}
+
+	col = value;
+}
+
+void ChessCoordinate::setRow(int value)
+{
+	if (!CommonUtils::isInRange(value, Constants::MIN_ROW_COORDINATE, Constants::BOARD_SIZE))
+	{
+		throw std::out_of_range("Row is out of range");
+	}
+
+	row = value;
+}
+
+bool ChessCoordinate::isSameRow(const ChessCoordinate& other) const
+{
+	return row == other.row;
+}
+
+bool ChessCoordinate::isSameCol(const ChessCoordinate& other) const
+{
+	return col == other.col;
+}
+
+bool ChessCoordinate::isSameDiagonal(const ChessCoordinate& other) const
+{
+	return col + row == other.col + other.row
+		|| col - row == other.col - other.row;
+}
+
+bool ChessCoordinate::isNeighborWith(const ChessCoordinate& other) const
+{
+	const int MIN_DIFF = -1;
+	const int MAX_DIFF = 1;
+
+	int rowsDiff = row - other.row;
+	int colsDiff = col - other.col;
+
+	return CommonUtils::isInRange(rowsDiff, MIN_DIFF, MAX_DIFF)
+		&& CommonUtils::isInRange(colsDiff, MIN_DIFF, MAX_DIFF);
+}
---
+++ ChessCoordinate.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Constants.h"
+
+struct ChessCoordinate
+{
+private:
+	int row = Constants::MIN_ROW_COORDINATE;
+	char col = Constants::MIN_COL_COORDINATE;
+
+public:
+	ChessCoordinate() = default;
+	ChessCoordinate(char col, int row);
+
+	int getRow() const;
+	char getCol() const;
+
+	void setRow(int value);
+	void setCol(char value);
+
+	bool isSameRow(const ChessCoordinate& other) const;
+	bool isSameCol(const ChessCoordinate& other) const;
+	bool isSameDiagonal(const ChessCoordinate& other) const;
+	bool isNeighborWith(const ChessCoordinate& other) const;
+};
