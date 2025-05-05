efcb8f9041ef56ae646bc635aaa89a83f1e03c28
VelinaSouroujon <velinasouroujon@gmail.com>
2025-05-06 02:10:43 +0300

    Add getters and setters for piece position

--- Piece.cpp
+++ Piece.cpp
@@ -4,3 +4,13 @@ PieceColor Piece::getColor() const
 {
     return color;
 }
+
+ChessCoordinate Piece::getPosition() const
+{
+    return position;
+}
+
+void Piece::setPosition(const ChessCoordinate& newPosition)
+{
+    position = newPosition;
+}
