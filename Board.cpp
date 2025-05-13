f6d298202688d9ed18e95c964b639fd6529afb63
VelinaSouroujon <velinasouroujon@gmail.com>
2025-05-13 21:34:44 +0300

    Add validation that there are no pieces between two chess coordinates

--- Board.cpp
+++ Board.cpp
@@ -2,33 +2,173 @@
 #include "Board.h"
 #include "CommonUtils.h"
 
-int Board::getBoardRowIdx(int row) const
+int Board::getBoardRowIdx(const ChessCoordinate& coordinate) const
 {
-	return getSize() - row;
+	return getSize() - coordinate.getRow();
 }
 
-int Board::getBoardColIdx(char col) const
+int Board::getBoardColIdx(const ChessCoordinate& coordinate) const
 {
-	return col - Constants::MIN_COL_COORDINATE;
+	return coordinate.getCol() - Constants::MIN_COL_COORDINATE;
+}
+
+bool Board::isPieceBetweenSameRow(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const
+{
+	if (!firstCoord.isSameRow(secondCoord))
+	{
+		return false;
+	}
+
+	int row = firstCoord.getRow();
+	char firstCol = firstCoord.getCol();
+	char secondCol = secondCoord.getCol();
+
+	if (firstCol > secondCol)
+	{
+		std::swap(firstCol, secondCol);
+	}
+
+	for (int col = firstCol + 1; col < secondCol; col++)
+	{
+		if (at(col, row) != nullptr)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Board::isPieceBetweenSameCol(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const
+{
+	if (!firstCoord.isSameCol(secondCoord))
+	{
+		return false;
+	}
+
+	int firstRow = firstCoord.getRow();
+	int secondRow = secondCoord.getRow();
+	char col = firstCoord.getCol();
+
+	if (firstRow > secondRow)
+	{
+		std::swap(firstRow, secondRow);
+	}
+
+	for (int row = firstRow + 1; row < secondRow; row++)
+	{
+		if (at(col, row) != nullptr)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Board::isPieceBetweenSameDiagonalLeftToRight(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const
+{
+	if (!firstCoord.isSameDiagonalLeftToRight(secondCoord))
+	{
+		return false;
+	}
+
+	char col = firstCoord.getCol();
+	int firstRow = firstCoord.getRow();
+	int secondRow = secondCoord.getRow();
+
+	if (firstRow < secondRow)
+	{
+		std::swap(firstRow, secondRow);
+		col = secondCoord.getCol();
+	}
+
+	for (int row = firstRow - 1; row > secondRow; row--)
+	{
+		col++;
+
+		if (at(col, row) != nullptr)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Board::isPieceBetweenSameDiagonalRightToLeft(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const
+{
+	if (!firstCoord.isSameDiagonalRightToLeft(secondCoord))
+	{
+		return false;
+	}
+
+	char col = firstCoord.getCol();
+	int firstRow = firstCoord.getRow();
+	int secondRow = secondCoord.getRow();
+
+	if (firstRow < secondRow)
+	{
+		std::swap(firstRow, secondRow);
+		col = secondCoord.getCol();
+	}
+
+	for (int row = firstRow - 1; row > secondRow; row--)
+	{
+		col--;
+
+		if (at(col, row) != nullptr)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Board::isPieceBetweenSameDiagonal(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const
+{
+	return isPieceBetweenSameDiagonalLeftToRight(firstCoord, secondCoord)
+		|| isPieceBetweenSameDiagonalRightToLeft(firstCoord, secondCoord);
 }
 
 const Piece* Board::at(const ChessCoordinate& coordinate) const
 {
-	int rowIdx = getBoardRowIdx(coordinate.getRow());
-	int colIdx = getBoardColIdx(coordinate.getCol());
+	int rowIdx = getBoardRowIdx(coordinate);
+	int colIdx = getBoardColIdx(coordinate);
 
 	return board[rowIdx][colIdx];
 }
 
 Piece*& Board::at(const ChessCoordinate& coordinate)
 {
-	int rowIdx = getBoardRowIdx(coordinate.getRow());
-	int colIdx = getBoardColIdx(coordinate.getCol());
+	int rowIdx = getBoardRowIdx(coordinate);
+	int colIdx = getBoardColIdx(coordinate);
 
 	return board[rowIdx][colIdx];
 }
 
+const Piece* Board::at(char col, int row) const
 {
+	ChessCoordinate coordinate(col, row);
+
+	return at(coordinate);
+}
+
+Piece*& Board::at(char col, int row)
+{
+	ChessCoordinate coordinate(col, row);
+
+	return at(coordinate);
+}
+
+bool Board::isPieceBetween(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const
+{
+	return isPieceBetweenSameCol(firstCoord, secondCoord)
+		|| isPieceBetweenSameRow(firstCoord, secondCoord)
+		|| isPieceBetweenSameDiagonal(firstCoord, secondCoord);
+}
+
 bool Board::removeAt(const ChessCoordinate& coordinate)
 {
 	if (at(coordinate) == nullptr)
--- Board.h
+++ Board.h
@@ -9,14 +9,23 @@ class Board
 private:
 	Piece* board[Constants::BOARD_SIZE][Constants::BOARD_SIZE]{ nullptr };
 
-	int getBoardRowIdx(int row) const;
-	int getBoardColIdx(char col) const;
+	int getBoardRowIdx(const ChessCoordinate& coordinate) const;
+	int getBoardColIdx(const ChessCoordinate& coordinate) const;
+
+	bool isPieceBetweenSameRow(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const;
+	bool isPieceBetweenSameCol(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const;
+	bool isPieceBetweenSameDiagonalLeftToRight(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const;
+	bool isPieceBetweenSameDiagonalRightToLeft(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const;
+	bool isPieceBetweenSameDiagonal(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const;
 
 public:
 	const Piece* at(const ChessCoordinate& coordinate) const;
 	Piece*& at(const ChessCoordinate& coordinate);
-	void removeAt(const ChessCoordinate& coordinate);
 
+	const Piece* at(char col, int row) const;
+	Piece*& at(char col, int row);
+
+	bool isPieceBetween(const ChessCoordinate& firstCoord, const ChessCoordinate& secondCoord) const;
 	bool removeAt(const ChessCoordinate& coordinate);
 	int getSize() const;
 };
