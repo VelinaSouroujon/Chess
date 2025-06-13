657aea510e3485e79e4a032233d0dbab182df679
VelinaSouroujon <velinasouroujon@gmail.com>
2025-06-14 01:57:19 +0300

    All game related info is in Game class

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,49 @@
+#include <stdexcept>
 #include "Game.h"
 #include "EnPassantNotifier.h"
+
+Game::Game(ChessVariant& chessVariant, RulesEngine& rulesEngine) 
+    : chessVariant(chessVariant),
+    rulesEngine(rulesEngine),
+    white(PieceColor::White, chessVariant.getBoard()),
+    black(PieceColor::Black, chessVariant.getBoard())
+{
+
+}
+
+const PiecesGameInfo& Game::playingSide() const
+{
+    return turnToMove == PieceColor::White
+        ? white
+        : black;
+}
+
+const PiecesGameInfo& Game::awaitingSide() const
+{
+    return turnToMove == PieceColor::White
+        ? black
+        : white;
+}
+
+const PiecesGameInfo& Game::sameColorPiecesInfo(PieceColor color) const
+{
+    return color == PieceColor::White
+        ? white
+        : black;
+}
+
+const PiecesGameInfo& Game::oppositeColorPiecesInfo(PieceColor color) const
+{
+    return color == PieceColor::White
+        ? black
+        : white;
+}
+
+const ChessVariant& Game::getChessVariant() const
+{
+    return chessVariant;
+}
+
 const ChessCoordinate* Game::getEnPassantSquare() const
 {
     if (enPassantState == EnPassantState::None)
@@ -9,6 +53,52 @@ const ChessCoordinate* Game::getEnPassantSquare() const
 
     return &enPassantSquare;
 }
+
+PiecesGameInfo& Game::playingSide()
+{
+    return turnToMove == PieceColor::White
+        ? white
+        : black;
+}
+
+PiecesGameInfo& Game::awaitingSide()
+{
+    return turnToMove == PieceColor::White
+        ? black
+        : white;
+}
+
+PiecesGameInfo& Game::sameColorPiecesInfo(PieceColor color)
+{
+    return color == PieceColor::White
+        ? white
+        : black;
+}
+
+PiecesGameInfo& Game::oppositeColorPiecesInfo(PieceColor color)
+{
+    return color == PieceColor::White
+        ? black
+        : white;
+}
+
+PieceColor Game::getTurnToMove() const
+{
+    return turnToMove;
+}
+
+PieceColor Game::getAwaitingSideColor() const
+{
+    return turnToMove == PieceColor::White
+        ? PieceColor::Black
+        : PieceColor::White;
+}
+
+ChessVariant& Game::getChessVariant()
+{
+    return chessVariant;
+}
+
 void Game::changeTurn()
 {
     turnToMove =
@@ -29,4 +119,9 @@ void Game::setEnPassantSquare(const ChessCoordinate& enPassantSquare)
 {
     this->enPassantSquare = enPassantSquare;
     enPassantState = EnPassantState::JustSet;
+}
+
+const RulesEngine& Game::rules() const
+{
+    return rulesEngine;
 }
--- Game.h
+++ Game.h
@@ -1,15 +1,47 @@
 #pragma once
 
-#include "Piece.h"
-#include "Constants.h"
-#include "Board.h"
+#include "ChessVariant.h"
+#include "PiecesGameInfo.h"
+#include "RulesEngine.h"
+
+class BinaryGameSerializer;
 
 class Game
 {
+private:
+	ChessVariant& chessVariant;
+	RulesEngine& rulesEngine;
+
+	PiecesGameInfo white;
+	PiecesGameInfo black;
+
+	PieceColor turnToMove = PieceColor::White;
+
 	ChessCoordinate enPassantSquare;
 	EnPassantState enPassantState = EnPassantState::None;
+
 public:
-	Board board;
+	friend class BinaryGameSerializer;
+
+	Game(ChessVariant& chessVariant, RulesEngine& rulesEngine);
+
+	const PiecesGameInfo& playingSide() const;
+	const PiecesGameInfo& awaitingSide() const;
+	const PiecesGameInfo& sameColorPiecesInfo(PieceColor color) const;
+	const PiecesGameInfo& oppositeColorPiecesInfo(PieceColor color) const;
+
+	PieceColor getTurnToMove() const;
+	PieceColor getAwaitingSideColor() const;
+	const ChessVariant& getChessVariant() const;
+	const ChessCoordinate* getEnPassantSquare() const;
+	const RulesEngine& rules() const;
+
+	PiecesGameInfo& playingSide();
+	PiecesGameInfo& awaitingSide();
+	PiecesGameInfo& sameColorPiecesInfo(PieceColor color);
+	PiecesGameInfo& oppositeColorPiecesInfo(PieceColor color);
+	ChessVariant& getChessVariant();
 	void changeTurn();
+	void setEnPassantSquare(const ChessCoordinate& enPassantSquare);
 };
 
