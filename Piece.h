e34e027aca2e9cf24468b13db91a17c2a6e741bb
VelinaSouroujon <velinasouroujon@gmail.com>
2025-06-15 02:14:02 +0300

    Add abstract methods

--- Piece.h
+++ Piece.h
@@ -19,10 +19,13 @@ public:
 	virtual char getPieceNotation() const = 0;
 	PieceColor getColor() const;
 
+	virtual bool isSufficientForWin() const = 0;
 	virtual bool isValidMove(const Move& move) const = 0;
+	virtual void fillDirections(Array<PieceDirection, Constants::MAX_DIRECTIONS_COUNT>& directions) const = 0;
 	virtual wchar_t accept(const PieceRenderer& renderer) const = 0;
 
 	ChessCoordinate getPosition() const;
 	void setPosition(const ChessCoordinate& newPosition);
+	virtual Piece* clone() const = 0;
 };
 
